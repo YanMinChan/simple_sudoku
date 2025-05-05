a670bf7eea74fa23bde4afa042e7bf1f004e4f41
Yan Min <yc89@hw.ac.uk>
2025-05-05 17:50:45 +0100

    set up grid class

---
+++ src/grid.cs
@@ -0,0 +1,13 @@
+namespace Sudoku{
+    // The 9x9 grid for Sudoku
+    public class Grid
+    {
+        int[] numbers;
+
+        // The constructor
+        public Grid()
+        {
+
+        }
+    }
+}
