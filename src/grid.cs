de405d77a53b4a0b8b2de441c375607436a363c5
YanMinChan <yc89@hw.ac.uk>
2025-05-06 20:23:40 +0100

    Fix error for successful demo build

--- src/grid.cs
+++ src/grid.cs
@@ -22,7 +22,7 @@ namespace Sudoku
         // Print the grid
         public string printGrid()
         {
-
+            return "";
         }
     }
 }
--- src/logic.cs
+++ src/logic.cs
@@ -3,6 +3,8 @@ namespace Sudoku
     // Defines the game logic
     public class Logic
     {
-
+        public static void Main(){
+            Console.WriteLine("Hello");
+        }
     }
 }
