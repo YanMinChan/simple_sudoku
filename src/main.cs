656f4824cd3319fd2a26d9a49012e41657348e3d
YanMinChan <yc89@hw.ac.uk>
2025-05-14 08:22:48 +0200

    Add read csv

--- src/main.cs
+++ src/main.cs
@@ -2,19 +2,30 @@ using System.IO;
 
 namespace Sudoku
 {
-    public class Main{
-        // Read from csv
-        using (StreamReader sr = new StreamReader("TestFile.txt"))
-        {
-            List<string> puzzles;
-            int i = 0;
-            while ((line = sr.ReadLine() != null) && i < 100)
-            {
+    public class Reader{
+        // Instance variable
+        List<string> puzzle;
+        List<string> solution;
 
+        public void ReadCSV(){
+            // Read from csv
+            using (StreamReader sr = new StreamReader("./dataset/sudoku.csv"))
+            {
+                string[] gameSet;
+                int i = 0;
+                string data;
+                while ((data = sr.ReadLine() != null) && i < 100)
+                {
+                    gameSet = data.Split(',');
+                    this.puzzle.Add(gameSet[0]);
+                    this.solution.Add(gameSet[1]);
+                    i++;
+                }
             }
         }
+
         public static void Main(){
-            Grid grid = new Grid(,);
+            Grid grid = new Grid(puzzle[0],solution[0]);
             Console.WriteLine(grid.printGrid());
         }
     }
