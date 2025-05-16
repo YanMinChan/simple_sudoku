82651d0aedbe72baa0bd15cb5d492ed5c09498a1
YanMinChan <yc89@hw.ac.uk>
2025-05-16 23:06:25 +0100

    Add csv reader and test print puzzle from csv

--- src/main.cs
+++ src/main.cs
@@ -2,31 +2,18 @@ using System.IO;
 
 namespace Sudoku
 {
-    public class Reader{
-        // Instance variable
-        List<string> puzzle;
-        List<string> solution;
+public class main{
+    public static void Main(){
+        // Testing sr
+        Reader r = new Reader();
+        r.ReadCSV("./dataset/sudoku.csv");
 
-        public void ReadCSV(){
-            // Read from csv
-            using (StreamReader sr = new StreamReader("./dataset/sudoku.csv"))
-            {
-                string[] gameSet;
-                int i = 0;
-                string data;
-                while ((data = sr.ReadLine() != null) && i < 100)
-                {
-                    gameSet = data.Split(',');
-                    this.puzzle.Add(gameSet[0]);
-                    this.solution.Add(gameSet[1]);
-                    i++;
-                }
-            }
-        }
+        // Saving puzzle to List
+        List<int[]> puz = r.Puzzle;
+        List<int[]> sol = r.Solution;
 
-        public static void Main(){
-            Grid grid = new Grid(puzzle[0],solution[0]);
-            Console.WriteLine(grid.printGrid());
-        }
+        Grid grid = new Grid(puz[0],sol[0]);
+        Console.WriteLine(grid.printGrid());
     }
+}
 }
---
+++ src/reader.cs
@@ -0,0 +1,56 @@
+using System.IO;
+
+namespace Sudoku
+{
+    public class Reader{
+        // Instance variable
+        List<int[]> puzzle;
+        List<int[]> solution;
+
+        // Constructor
+        public Reader(){
+            this.puzzle = new List<int[]>();
+            this.solution = new List<int[]>();
+        }
+
+        public void ReadCSV(string filePath){
+            // Read from csv
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string[] gameSet; //puzzle and solution
+                int i = 0; //limit num of puzzle read
+
+                string header = sr.ReadLine(); // skip the header
+                string data = sr.ReadLine();
+                while ((data != null) && i < 100)
+                {
+                    // obtain the puzzle and solution
+                    gameSet = data.Split(',');
+                    int[] puzSet = new int[81];
+                    int[] solSet = new int[81];
+                    // parse puzzle and solution to array
+                    for (int j = 0; j < 81; j++){
+                        // convert char to int by ascii
+                        puzSet[j] = gameSet[0][j] - '0';
+                        solSet[j] = gameSet[1][j] - '0';
+                    //Console.WriteLine("This is gameset " + gameSet[0][j]); // test
+                    //Console.WriteLine("This is puzSet " + puzSet[j]);
+                    }
+                    this.puzzle.Add(puzSet);
+                    this.solution.Add(solSet);
+                    i++;
+                }
+            }
+        }
+
+        public List<int[]> Puzzle {
+            get {return puzzle;}
+            set {puzzle = value;}
+        }
+
+        public List<int[]> Solution {
+            get {return solution;}
+            set{solution = value;}
+        }
+    }
+}
